dae0943eb88930c65f52e8f659567d42d0117036
许铎 <xuduo@yy.com>
2016-07-15 10:02:38 +0800

    fix bugs

--- android-push-sdk/src/main/java/com/yy/httpproxy/service/DelegateToClientNotificationHandler.java
+++ android-push-sdk/src/main/java/com/yy/httpproxy/service/DelegateToClientNotificationHandler.java
@@ -1,11 +1,6 @@
 package com.yy.httpproxy.service;
 
-import android.app.Notification;
-import android.app.NotificationManager;
-import android.app.PendingIntent;
 import android.content.Context;
-import android.content.Intent;
-import android.support.v4.app.NotificationCompat;
 
 /**
  * Created by xuduo on 11/6/15.
@@ -15,13 +10,11 @@ public class DelegateToClientNotificationHandler extends DefaultNotificationHand
 
     @Override
     public void handlerNotification(Context context, boolean binded, PushedNotification pushedNotification) {
-
         if (!binded) {
             showNotification(context, pushedNotification);
+        } else {
+            sendArrived(context, pushedNotification);
         }
-
-        sendArrived(context, pushedNotification);
-
     }
 
 }
