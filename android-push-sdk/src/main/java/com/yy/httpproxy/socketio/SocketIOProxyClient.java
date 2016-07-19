480c115919c95ba59dda4eb38808420f62a552e0
许铎 <xuduo@yy.com>
2016-07-19 21:14:17 +0800

    fix build

--- android-push-sdk/src/main/java/com/yy/httpproxy/socketio/SocketIOProxyClient.java
+++ android-push-sdk/src/main/java/com/yy/httpproxy/socketio/SocketIOProxyClient.java
@@ -27,7 +27,6 @@ import java.security.cert.CertificateException;
 import java.security.cert.X509Certificate;
 import java.util.HashMap;
 import java.util.Map;
-import java.util.Set;
 import java.util.logging.Level;
 
 import javax.net.ssl.HostnameVerifier;
