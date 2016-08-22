73c28aac7a73c7930a1247c4fcd75d6f122b4bfc
huangzhilong <964123660@qq.com>
2016-08-22 16:59:06 +0800

    fix hauwei bug

--- android-push-sdk/src/main/java/com/yy/httpproxy/thirdparty/ProviderFactory.java
+++ android-push-sdk/src/main/java/com/yy/httpproxy/thirdparty/ProviderFactory.java
@@ -1,6 +1,7 @@
 package com.yy.httpproxy.thirdparty;
 
 import android.content.Context;
+
 import com.yy.httpproxy.util.Log;
 
 import com.yy.httpproxy.thirdparty.HuaweiProvider;
@@ -13,10 +14,12 @@ public class ProviderFactory {
     private static final String KEY_HUAWEI_VERSION = "ro.confg.hw_systemversion";
     private static final String KEY_MIUI_VERSION = "ro.miui.ui.version.name";
     private static final String TAG = "ProviderFactory";
+    private static final String HUAWEI_BUG_NAME = "NXT-AL10";
+    private static final String HUAWEI_BUG_VERSION = "EmotionUI_4.1";
 
     public static NotificationProvider getProvider(Context context) {
         final SystemProperty prop = new SystemProperty(context);
-        if (isSystem(prop, KEY_HUAWEI_VERSION) && HuaweiProvider.available(context)) {
+        if (isSystem(prop, KEY_HUAWEI_VERSION) && HuaweiProvider.available(context) && !huaweiBug(prop)) {
             Log.i(TAG, "HuaweiProvider");
             return new HuaweiProvider(context);
         } else if (isSystem(prop, KEY_MIUI_VERSION) && XiaomiProvider.available(context)) {
@@ -33,6 +36,12 @@ public class ProviderFactory {
         boolean b = value != null && !value.isEmpty();
         Log.d(TAG, key + " " + value + " " + b);
         return b;
+    }
+
+    private static boolean huaweiBug(SystemProperty prop) {
+        String productName = prop.get("ro.product.name");
+        String emuiVersion = prop.get("ro.build.version.emui");
+        return HUAWEI_BUG_VERSION.equals(emuiVersion) && HUAWEI_BUG_NAME.equals(productName);
     }
 
 }
