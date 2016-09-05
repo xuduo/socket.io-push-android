62c2c02b802bd0d82f67d1b06e74e0356617090f
huangzhilong <964123660@qq.com>
2016-09-05 15:24:09 +0800

    开机启动

--- android-push-sdk/src/main/java/com/yy/httpproxy/service/BootBroadcastReceiver.java
+++ android-push-sdk/src/main/java/com/yy/httpproxy/service/BootBroadcastReceiver.java
@@ -4,13 +4,17 @@ import android.content.BroadcastReceiver;
 import android.content.Context;
 import android.content.Intent;
 
+import com.yy.httpproxy.util.Log;
+
 /**
  * Created by Administrator on 2015/12/31.
  */
 public class BootBroadcastReceiver extends BroadcastReceiver {
+    private static String TAG = "MyBootBroadcastReceiver";
 
     @Override
     public void onReceive(Context context, Intent intent) {
+        Log.i(TAG, "BOOT_COMPLETED start ConnectService");
         Intent service = new Intent(context, ConnectionService.class);
         context.startService(service);
     }
